1692dfe3dcf7e92fd4261ed26a365b8cad576bd1
tanuja <tanujamurali012@gmail.com>
2024-11-12 15:25:12 +0530

    intell

---
+++ src/InsertionSort.java
@@ -0,0 +1,33 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
